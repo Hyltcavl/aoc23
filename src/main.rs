b5a3ef47ace266ef3f5e63b32501c8a071f6705c
Teodor Englund <teodor.englund@gmail.com>
2023-12-01 09:47:19 +0000

    star 1 done, moving on to star 2

--- src/main.rs
+++ src/main.rs
@@ -26,3 +26,29 @@ fn main() {
     }
     print!("{}", big_num);
 }
+
+fn star_one() {
+    let file_contents =
+        fs::read_to_string("input.txt").expect("LogRocket: Should have been able to read the file");
+    let content_list: Vec<_> = file_contents.lines().collect();
+    println!("{:#?}", content_list[2]);
+
+    let mut big_num: i32 = 0;
+
+    for s in content_list {
+        //let listOfNumbers: Vec<char> = s.split(char::is_numeric);
+        //let nums: Vec<_> =
+        let nums: Vec<char> = s.chars().filter(|x| x.is_numeric()).collect();
+
+        println!("{}", s);
+
+        if nums.first().is_some() && nums.last().is_some() {
+            let first: String = nums.first().unwrap().to_string();
+            let second: String = nums.last().unwrap().to_string();
+            let total = format!("{first}{second}");
+            println!("{}", total);
+            let my_int: i32 = total.parse().unwrap();
+            big_num = big_num.add(my_int);
+        }
+    }
+}
